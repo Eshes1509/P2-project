0350d13548f6dd33b10d6ac1a317040b2596e88a
Thomas Bjeldbak Madsen <thomaseshes@gmail.com>
2022-04-12 12:00:58 +0200

    Questionnaire done

--- myapp/app.js
+++ myapp/app.js
@@ -8,8 +8,8 @@
 let createError = require("http-errors");
 let express = require("express");
 let path = require("path");
-let cookieParser = require("cookie-parser");
-let logger = require("morgan");
+// let cookieParser = require("cookie-parser");
+// let logger = require("morgan");
 
 let indexRouter = require("./routes/index");
 let usersRouter = require("./routes/users");
