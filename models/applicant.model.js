85a92198a5314a1a06019387634541765940f176
Toooody <Tobias.c.h@hotmail.com>
2022-05-09 12:54:31 +0200

    dwanob

--- models/applicant.model.js
+++ models/applicant.model.js
@@ -1,5 +1,69 @@
 const mongoose = require('mongoose');
 
+let formSchema = new mongoose.Schema({
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    gender: {
+        type: Number
+    },
+    email: {
+        type: String
+    },
+    age: {
+        type: Number
+    },
+    major:{
+        type: String
+    },
+    semester:{
+        type: Number
+    },
+    roommateLanguage:{
+        type: String
+    },
+    planVisitors:{
+        type: Number
+    },
+    preferStudy:{
+        type: Number
+    },
+    bedtime:{
+        type: Number
+    },
+    roommateGender:{
+        type: Number
+    },
+    preferInternational:{
+        type: String
+    },
+    preferedAgeRange:{
+        type: String
+    },
+    btnradio4:{
+        type: String
+    },
+    roommateMajor:{
+        type: String
+    },
+    roommateSemester:{
+        type: String
+    },
+    budget:{
+        type: Number
+    },
+    btnradio7:{
+        type: String
+    },
+    aalborgSection:{
+        type: String
+    }
+
+
+})
 
 
 
@@ -7,16 +71,18 @@ const mongoose = require('mongoose');
 
 
 
-
-
+/* 
 
 const formSchema = new mongoose.Schema(
     {
+
+
+
         data: Object,
     },
     //{ collection: "Answers" }
 );
-
+ */
 const Form = mongoose.model("Form", formSchema);
 /* 
 const formData = bodyData => {
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -23,7 +23,30 @@ app.get("/", function (request, response) {
 
 function insertData(request, response) {
     let applicant = new Applicant();
-    applicant = request.body;
+    applicant.firstName = request.body.firstName;
+    applicant.lastName = request.body.lastName;
+    applicant.gender = request.body.gender;
+    applicant.email = request.body.email;
+    applicant.age = request.body.age;
+    applicant.major = request.body.major;
+    applicant.semester = request.body.semester;
+    applicant.roommmateLanguege = request.body.roommateLanguage;
+    applicant.planVisitorsRoommate = request.body.planVisitorsRoommate;
+    applicant.planVisitors = request.body.planVisitors;
+    applicant.preferStudy = request.body.preferStudy;
+    applicant.bedtime = request.body.bedtime;
+    applicant.roommateGender = request.body.roommateGender;
+    applicant.preferInternational = request.body.preferInternational;
+    applicant.preferedAgeRange = request.body.preferedAgeRange;
+    applicant.btnradio4 = request.body.btnradio4;
+    applicant.roommateMajor = request.body.roommateMajor;
+    applicant.roommateSemester = request.body.roommateSemester;
+    applicant.budget = request.body.budget;
+    applicant.btnradio7 = request.body.btnradio7;
+    applicant.aalborgSection = request.body.aalborgSection;
+
+
+
     applicant.save((err, doc) => {
         if(!err) 
             response.redirect('pages/results');
