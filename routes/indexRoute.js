8b2f513c35a886e45d91cfaec646208441ba71d2
Thomas Bjeldbak Madsen <thomaseshes@gmail.com>
2022-05-09 10:53:45 +0200

    Added eslint in indexRoute.js

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,3 +1,13 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable space-infix-ops */
+/* eslint-disable prefer-const */
+/* eslint-disable no-undef */
+/* eslint-disable node/no-path-concat */
+/* eslint-disable indent */
+/* eslint-disable quotes */
+/* eslint-disable semi */
+/* eslint-disable no-unused-vars */
+
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
@@ -11,8 +21,8 @@ app.get("/", function (request, response) {
 
 app.post("/results", function (request, response) {
     return response.send(request.body);
-     response.render("pages/results.ejs");
-}); 
+    response.render("pages/results.ejs");
+});
 
 app.get("/guide", function (request, response) {
     return response.render("pages/guide.ejs");
@@ -27,6 +37,5 @@ app.get("/test", (req, res) => {
             res.send(result);
         });
 });
-
 
 module.exports = app;
