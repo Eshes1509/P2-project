e11ed021e4a630975aa257a1b86429c3df296295
Thomas Bjeldbak Madsen <thomaseshes@gmail.com>
2022-05-11 11:55:33 +0200

    Added algorithms to indexRoute.js

--- app.js
+++ app.js
@@ -10,7 +10,6 @@
 
 const PORT = process.env.PORT || 5000;
 
-require("./models/db");
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
@@ -23,21 +22,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views/"));
 app.use(express.static("public"));
 
-// Connection URI
-const uri =
-    "mongodb+srv://test:test@cluster0.uwjpd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
-// Create a new MongoClient
-const mongoclient = new MongoClient(uri);
-
-let db = mongoclient.db("");
-
 app.use("/", indexRoute);
-
-app.use(function (req, res, next) {
-    req.db = db;
-    next();
-});
 
 app.use("/results", indexRoute);
 
