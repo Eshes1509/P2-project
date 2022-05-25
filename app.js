dae3652dee58fba5cf819d833bb1320b21981652
Thomas Bjeldbak Madsen <thomaseshes@gmail.com>
2022-05-25 10:43:56 +0200

    Deleted unwanted code in app.js

--- app.js
+++ app.js
@@ -12,9 +12,6 @@ const PORT = process.env.PORT || 5000;
 
 const express = require("express");
 const app = express();
-const server = require("http").Server(app);
-const ejs = require("ejs");
-const MongoClient = require("mongodb").MongoClient;
 const path = require("path");
 const indexRoute = require("./routes/indexRoute");
 
